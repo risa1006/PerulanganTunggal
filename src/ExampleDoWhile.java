dfb7910efb90eff57ce4cf26c6ca6b0de15a0627
risa1006 <risa@unida.ac.id>
2021-12-18 11:19:05 +0700

    JavaPT
    belajar perulangan tunggal

---
+++ src/ExampleDoWhile.java
@@ -0,0 +1,18 @@
+
+public class ExampleDoWhile {
+	public static void main (String[] args) {
+		int number = 0;
+		do {
+			System.out.println("Angka Lebih Besar Dari 10");
+		}while(number>10);
+		System.out.println("Pernyataan ini berasal dari luar while loop");
+	}
+}
+
+
+
+
+
+
+
+
---
+++ src/Examplewhile.java
@@ -0,0 +1,17 @@
+
+public class Examplewhile {
+	public static void main (String[] args) {
+		int number = 0;
+		while (number> 10) {
+			System.out.println("Angka Lebih Besar Dari 10");
+		}
+		System.out.println("Pernyataan ini berasal dari luar while loop");
+	}
+}
+
+
+
+
+
+
+
